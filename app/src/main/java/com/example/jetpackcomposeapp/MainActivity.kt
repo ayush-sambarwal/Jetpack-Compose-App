5f6927d76cebb67b4254e90c919a7d2ac271c74d
ayush-sambarwal <ayush.sambarwal@internshala.com>
2024-03-27 15:34:59 +0530

    click counter app

--- app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.jetpackcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposeapp.ui.theme.JetpackComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +22,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                var count by remember {
+                    mutableIntStateOf(0)
+                }
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Greeting("Android")
+                    Text(
+                        text = count.toString(),
+                        fontSize = 30.sp
+                    )
+                    Button(onClick = {
+                        count++
+                    }) {
+                        Text(text = "Click me!")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeAppTheme {
-        Greeting("Android")
     }
 }
